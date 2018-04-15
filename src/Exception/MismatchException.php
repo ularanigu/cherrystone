5454dff178cfab6a7f053fed88cad2ffb67e918a
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-15 18:25:52 -0400

    Update MismatchException.php

--- src/Exception/MismatchException.php
+++ src/Exception/MismatchException.php
@@ -1,1 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
 
+namespace Ularanigu\Cherrystone\Exception;
+
+use Exception;
+
+/**
+ * @class MismatchException.
+ */
+class MismatchException extends Exception implements ExceptionInterface
+{
+}
