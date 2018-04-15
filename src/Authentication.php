f7dccd07b8f049f3c56fffe52ebf60e739b3759f
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-15 19:37:31 -0400

    Update Authentication.php

--- src/Authentication.php
+++ src/Authentication.php
@@ -1,1 +1,37 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
 
+namespace Ularanigu\Cherrystone;
+
+/**
+ * @class Authentication.
+ */
+class Authentication implements AuthenticationInterface
+{
+
+    /**
+     * @var string|null $format The authentication format.
+     */
+    private $format = \null;
+
+    /**
+     * Constructor.
+     *
+     * @param array $options The authentication options.
+     *
+     * @return void Return nothing.
+     */
+    public function __construct(array $options)
+    {
+        if (!empty($options)) {
+            $this->format = isset($options['format']) && \in_array($options['format'], \AUTHENTICATION_FORMATS, true) ? $options : \null;
+        }
+    }
+}
