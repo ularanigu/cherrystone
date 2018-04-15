075e1beaf99d79907739343484c70729837d4ca1
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-15 19:23:30 -0400

    Create InvalidOperationException.php

---
+++ src/Exception/InvalidOperationException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
+
+namespace Ularanigu\Cherrystone\Exception;
+
+use RuntimeException;
+
+/**
+ * @class InvalidOperationException.
+ */
+class InvalidOperationException extends RuntimeException implements ExceptionInterface
+{
+}
