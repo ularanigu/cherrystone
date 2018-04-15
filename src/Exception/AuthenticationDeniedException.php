874e3a9fa188d18b24fe3c2f45aad142fa7bba29
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-15 18:24:24 -0400

    Create AuthenticationDeniedException.php

---
+++ src/Exception/AuthenticationDeniedException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
+
+namespace Ularanigu\Cherrystone\Exception;
+
+use Exception;
+
+/**
+ * @class AuthenticationDeniedException.
+ */
+class AuthenticationDeniedException extends Exception implements ExceptionInterface
+{
+}
