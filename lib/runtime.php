c2f959a0bfa1c38bc2a71d9896705c7f01cce032
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-15 19:39:48 -0400

    Create runtime.php

---
+++ lib/runtime.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
+
+define('AUTHENTICATION_FORMATS', [
+    'form'
+]);
