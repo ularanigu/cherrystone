259b216b88656ef55d6f4ded92445440086b8053
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-17 19:50:49 -0400

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -1,1 +1,30 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
 
+function cs_check(): void
+{
+    if (!function_exists('get_browser')) {
+        trigger_error(
+            'The get_browser() function does not exists.',
+            E_USER_ERROR
+        )
+    } elseif (!file_exists(ini_get('browscap'))) {
+        trigger_error(
+            'The browscap leads to a file that does not exist.',
+            E_USER_ERROR
+        );
+    } elseif (strcmp('browscap.ini', ini_get('browscap')) !== 0) {
+        trigger_error(
+            'The browscap leads to a file that is not an ini configuration file or it has an invalid name.',
+            E_USER_ERROR
+        );
+    } else {
+    }
+}
