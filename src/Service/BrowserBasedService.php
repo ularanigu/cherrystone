b23eed3f58fd833d0f6ba492bdebfe6acf4c531d
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-17 16:41:15 -0400

    Update BrowserBasedService.php

--- src/Service/BrowserBasedService.php
+++ src/Service/BrowserBasedService.php
@@ -1,1 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
 
+namespace Ularanigu\Cherrystone\Service;
+
+use Ularanigu\Cherrystone\Classification\Service;
+
+/**
+ * @class BrowserBasedService.
+ */
+class BrowserBasedService extends Service
+{
+}
