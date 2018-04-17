0bb20029507bf98153321ae9e879c52fe10baf3f
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-17 16:22:54 -0400

    Update AuthenticationObject.php

--- src/Classification/AuthenticationObject.php
+++ src/Classification/AuthenticationObject.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
 
+namespace Ularanigu\Cherrystone\Classification;
+
+/**
+ * @interface AuthenticationObject.
+ */
+interface AuthenticationObject
+{
+}
