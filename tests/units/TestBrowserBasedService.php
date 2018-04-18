d8bc7e912c8f983eff11046b2ea67fb8a7e7ad5b
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-17 23:41:33 -0400

    Update TestBrowserBasedService.php

--- tests/units/TestBrowserBasedService.php
+++ tests/units/TestBrowserBasedService.php
@@ -1,1 +1,23 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
 
+use PHPUnit\Framework\TestCase;
+
+use Ularanigu\Cherrystone\Service\BrowserBasedService;
+
+class TestBrowserBasedService extends TestCase
+{
+    public function testConstructor()
+    {
+        $service1 = new BrowserBasedService([], 'my-alias');
+        $this->assertEquals($service1->alias, 'my-alias');
+      
+    }
+}
