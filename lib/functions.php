66dbe43cb629f23d37875ad8a3fac4bd89dba959
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-18 18:52:34 -0400

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -10,21 +10,25 @@ declare(strict_types=1);
 
 function cs_check_gb(): void
 {
-    if (!function_exists('get_browser')) {
-        trigger_error(
-            'The get_browser() function does not exists.',
-            E_USER_ERROR
-        );
-    } elseif (!file_exists(ini_get('browscap'))) {
-        trigger_error(
-            'The browscap leads to a file that does not exist.',
-            E_USER_ERROR
-        );
-    } elseif (strcmp('browscap.ini', ini_get('browscap')) !== 0) {
-        trigger_error(
-            'The browscap leads to a file that is not an ini configuration file or it has an invalid name.',
-            E_USER_ERROR
-        );
+    if (!class_exists('Ularanigu\Firestorm\Report\Active')) {
+        if (!function_exists('get_browser')) {
+            trigger_error(
+                'The get_browser() function does not exists.',
+                E_USER_ERROR
+            );
+        } elseif (!file_exists(ini_get('browscap'))) {
+            trigger_error(
+                'The browscap leads to a file that does not exist.',
+                E_USER_ERROR
+            );
+        } elseif (strcmp('browscap.ini', ini_get('browscap')) !== 0) {
+            trigger_error(
+                'The browscap leads to a file that is not an ini configuration file or it has an invalid name.',
+                E_USER_ERROR
+            );
+        } else {
+        }
     } else {
+        return 'use_firestorm_polyfill'
     }
 }
