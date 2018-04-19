abc35a9d571afc85140e9b0fb3bbe1a400dce0ff
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-19 11:00:03 -0400

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -7,3 +7,13 @@ declare(strict_types=1);
  * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
  * @link    <https://github.com/ularanigu/cherrystone>.
  */
+
+namespace Ularanigu\Cherrystone;
+
+/**
+ * @function version().
+ */
+function version(): string
+{
+    return \json_encode(array(\CHERRYSTONE_VERSION_ID, \CHERRYSTONE_VERSION));
+}
