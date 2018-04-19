5093a87befa78464317155488ea60e3964a9d5cc
Nicholas English <38263557+ularanigu@users.noreply.github.com>
2018-04-19 10:34:41 -0400

    Create InvalidArgumentException.php

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
+
+namespace Ularanigu\Cherrystone\Exception;
+
+use InvalidArgumentException as IAE;
+
+/**
+ * @class InvalidArgumentException.
+ */
+class InvalidArgumentException extends IAE implements ExceptionInterface
+{
+}
