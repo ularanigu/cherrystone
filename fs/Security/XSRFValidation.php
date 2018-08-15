0e59c8088a829222e4d4a6b58adca87269a0e394
Ashley <38263557+ularanigu@users.noreply.github.com>
2018-08-14 22:44:50 -0500

    Update and rename fs/XSRFValidation.php to fs/Security/XSRFValidation.php

---
+++ fs/Security/XSRFValidation.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
+
+namespace Ularanigu\Firestorm\Security;
+
+use Ularanigu\Firestorm\Utils;
+use Ularanigu\Firestorm\Exception\XSRFException;
+
+/**
+ * @class XSRFValidation.
+ */
+class XSRFValidation extends Utils
+{
+}
