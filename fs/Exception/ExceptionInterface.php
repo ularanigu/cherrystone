8a1f4e37ecdea2cfbe1bc609eec3564df549b9af
Ashley <38263557+ularanigu@users.noreply.github.com>
2018-08-14 22:32:41 -0500

    Add the exception interface.

--- fs/Exception/ExceptionInterface.php
+++ fs/Exception/ExceptionInterface.php
@@ -1,1 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Cherrystone Authentication.
+ * A more secure web.
+ *
+ * @license <https://github.com/ularanigu/cherrystone/blob/master/LICENSE>.
+ * @link    <https://github.com/ularanigu/cherrystone>.
+ */
 
+namespace Ularanigu\Firestorm\Exception;
+
+use Throwable;
+
+/**
+ * @interface ExceptionInterface.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
